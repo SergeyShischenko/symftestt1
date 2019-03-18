7578114c9dc5b5673c334daae3768f477f449da7
Sergey <odevaemdetok.kiev.ua@gmail.com>
2019-03-18 18:45:11 +0200

    All

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Notes;
 use App\Form\NoteType;
 use Doctrine\DBAL\Types\TextType;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,7 @@ class MainController extends AbstractController
         $form = $this->createForm(NoteType::class, $note);
         $form->handleRequest($request);
 
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'form'=>$form->createView(),
@@ -27,11 +29,13 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/artec/{page}", name="app_artec", requirements={"page"="\d+"})
+     * @Route("/artec/{page}", name="app_artec")
      */
-    public function artec($page=1){
+    public function artec($page, LoggerInterface $logger){
+
 
         return $this->render('main/artec.html.twig');
+
 
     }
 }
